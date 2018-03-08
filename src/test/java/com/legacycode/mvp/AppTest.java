e737f34d613cc0a1aea0ae2efa31efe20a5a0441
Christian Lehmann <info@legacycode.org>
2018-03-08 19:04:43 +0100

    migrated to junit5 and added stub test. removed mockito from project.

--- src/test/java/com/legacycode/mvp/AppTest.java
+++ src/test/java/com/legacycode/mvp/AppTest.java
@@ -1,33 +1,16 @@
 package com.legacycode.mvp;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
-	}
-
-	/**
-	 * Create the test case
-	 *
-	 * @param testName
-	 *            name of the test case
-	 */
-	public AppTest(String testName) {
-		super(testName);
-	}
+class AppTest {
 
 	/**
 	 * Rigourous Test :-)
 	 */
+	@Test
 	public void testApp() {
 		assert true;
 	}
